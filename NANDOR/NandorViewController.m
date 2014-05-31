40a003b62b6130c9eacdd10a17d44a9f7d70e2f8
Chris Nandor <cnandor@marchex.com>
2014-05-30 21:11:44 -0700

    Working button

--- NANDOR/NandorViewController.m
+++ NANDOR/NandorViewController.m
@@ -9,21 +9,42 @@
 #import "NandorViewController.h"
 
 @interface NandorViewController ()
-
+@property (strong, nonatomic) NSURLSessionConfiguration *sessionConfiguration;
+@property (strong, nonatomic) NSURLSession *session;
+@property (strong, nonatomic) NSURLCredential *sessionCredential;
+@property (nonatomic) BOOL sessionInited;
+@property (strong, nonatomic) NSString *baseURL;
 @end
 
 @implementation NandorViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+- (void)initSession {
+    if (self.sessionInited) {
+        return;
+    }
+    self.sessionConfiguration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    self.session = [NSURLSession sessionWithConfiguration:self.sessionConfiguration];
+    self.baseURL = @"http://XX:YY@ZZ/cgi-bin/horn2.cgi";
+    self.sessionInited = true;
+    return;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)makeSessionRequest:(NSString *)params {
+    NSURL *url = [NSURL URLWithString:self.baseURL];
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    [request setHTTPMethod:@"POST"];
+    [request setHTTPBody:[params dataUsingEncoding:NSUTF8StringEncoding]];
+
+    NSURLSessionDataTask *dataTask = [self.session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+    }];
+    [dataTask resume];
+}
+
+- (IBAction)pressHornButton:(UIButton *)sender {
+    [self initSession];
+    [self makeSessionRequest:@"quiet=1&horn=on&style=old&ajax=1"];
 }
 
 @end
+
+
